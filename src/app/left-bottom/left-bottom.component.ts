86807225f83a6519d54bc05459b9b73428dceeed
Userluckytian <tianlukang123@163.com>
2020-10-12 17:43:36 +0800

    feat:get请求

--- src/app/left-bottom/left-bottom.component.ts
+++ src/app/left-bottom/left-bottom.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 // 引入数据请求服务
-// import { ModuleDataRxInquireService } from '@cmss/core';
+import { ModuleDataRxInquireService } from '@cmss/core';
+import { HttpClient } from "@angular/common/http";
 
 @Component({
   selector: 'app-left-bottom',
@@ -10,12 +11,14 @@ import { Component, OnInit } from '@angular/core';
 export class LeftBottomComponent implements OnInit {
   data = '';
   // constructor(private dataRxInquireService: ModuleDataRxInquireService) { }
-  constructor() { }
+  constructor(public http: HttpClient) { }
 
   ngOnInit() {
     // this.dataRxInquireService.get('common', 'location.list', null, null).subscribe(res => {
     //   this.data = res;
     // });
+    var api = "http://a.itying.com/api/productlist";
+    this.http.get(api).subscribe(response => { console.log(response); });
   }
 
   showMsg() {
