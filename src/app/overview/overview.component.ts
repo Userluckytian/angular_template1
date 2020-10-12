9b2a5a8b88d9bba91b80b8ab86c32d862d7f2747
Userluckytian <tianlukang123@163.com>
2020-10-12 18:37:15 +0800

    fix：路由调整

--- src/app/overview/overview.component.ts
+++ src/app/overview/overview.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-overview',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./overview.component.scss']
 })
 export class OverviewComponent implements OnInit {
+  data = ['overview', 'mapboxmap'];
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
 
+
+
+  routerTo(index) {
+    const navTo = './' + this.data[index];
+    this.router.navigate([navTo], {
+      queryParams: {
+        id: index
+      }
+    });
+  }
 }
